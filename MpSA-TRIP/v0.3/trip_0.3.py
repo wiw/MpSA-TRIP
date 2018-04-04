7ee04dc86d422295d98c369e7128f6a65bc1512d
wiw <anton.ivankin@gmail.com>
2018-04-04 18:31:10 +0700

    template for main trip programm, import config for logging, make base for config generator

---
+++ MpSA-TRIP/v0.3/trip_0.3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+    trip_0.3.py
+    programm software version 0.3
+    Search for barcodes and associated mutations.
+    Report on the work done and statistics output.\n
+    author: Anton V. Ivankin
+    e-mail: anton.ivankin@gmail.com
+    source: https://github.com/wiw/MpSA-TRIP/
+"""
+
